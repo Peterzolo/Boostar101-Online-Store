740953925017fd1d031d7ddca6323174e887b4c6
Peter Solomon <petersolomon704@gmail.com>
2021-09-28 04:46:40 +0100

    Making a get request from the backend

---
+++ src/components/message/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div>
+      <i className="fa fa-spinner fa-spin"></i>
+      Loading products ...
+    </div>
+  );
+};
+
+export default Loading;
--- src/components/screens/HomeScreen.js
+++ src/components/screens/HomeScreen.js
@@ -1,13 +1,45 @@
 import React from "react";
-import data from "../../data";
 import Product from "../Product";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Loading from "../message/Loading";
+import Message from "../message/Message";
+//import data from '../../data'
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get("http://localhost:5000/api/products");
+        console.log("This is the data", data);
+        setProducts(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="row center">
-      {data.products.map((product) => (
-        <Product key={product._id} product={product} />
-      ))}
+    <div>
+      {loading ? (
+        <Loading></Loading>
+      ) : error ? (
+        <Message variant ="danger">{error}</Message>
+      ) : (
+        <div className="row center">
+          {products.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
